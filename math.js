c55cf7b1ec996ca30fb0c21ac3d1c89b2b1031fc
reza-taubat <echa8150@gmail.com>
2024-10-08 20:06:40 +0700

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+function tambah(a, b) {
+    return a + b;
+}
+function kali(a, b) {
+    return a * b;
+}
+function bagi(a, b) {
+    if (b === 0)
+        throw new Error("Tidak bisa membagi dengan nol");
+    return a / b;
+}
+function kurang(a, b) {
+    return a - b;
+}
+module.exports = { tambah, kali, bagi, kurang };
